4406daebadceba5d2c20adb2b8afb46301744060
doanquocdat813 <datcl24v7x224@dttx.ctu.edu.vn>
2025-03-07 20:23:24 +0700

    Add ForLoopExample

---
+++ ForLoopExample.java
@@ -0,0 +1,9 @@
+public class ForLoopExample {
+    public static void main(String[] args) {
+        // Vòng lặp for từ 1 đến 10
+        for (int i = 1; i <= 10; i++) {
+            // Hiển thị giá trị của i
+            System.out.println(i);
+        }
+    }
+}
